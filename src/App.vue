50136d5285b996f9320523138d4fbb93830e68f7
elciosava <elciosava@outlook.com>
2024-11-25 15:56:00 -0300

    gsgs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Cabecalho_c from './components/cabecalho_c.vue';
 </script>
 
 <template>
+  <Cabecalho_c />
   <RouterLink />
   <RouterView />
 </template>
--- src/components/menu_c.vue
+++ src/components/menu_c.vue
@@ -3,9 +3,24 @@
 </script>
 
 <template>
-
+    <div id="container">
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </template>
 
-<style scoped> 
-
-</style>
+<style scoped></style>
