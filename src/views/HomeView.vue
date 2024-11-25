9928d911560ac3742b62b0870b53791f041f3d21
elciosava <elciosava@outlook.com>
2024-11-25 15:51:20 -0300

    née

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,22 @@
 </script>
 
 <template>
-
+    <div id="container">
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </template>
