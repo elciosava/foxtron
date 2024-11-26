b73c15be5aae452b5d0d81d3fef8c60bd1a9848a
elciosava <elciosava@outlook.com>
2024-11-26 15:54:34 -0300

    hhjgh

--- src/components/menu_c_celular.vue
+++ src/components/menu_c_celular.vue
@@ -31,7 +31,7 @@ onMounted(() => {
                 </button>
                 <nav v-if="isMenuOpen">
                     <ul>
-                        <li><a href="#home">Home</a></li>
+                        <li><a href="index.html">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#services">Services</a></li>
                         <li><a href="#contact">Contact</a></li>
